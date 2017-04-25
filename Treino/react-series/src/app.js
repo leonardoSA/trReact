5087fe6e828cf2e978a039753f85fc90ee1b2235
Leonardo Araujo <leocrazy007@hotmail.com>
2017-04-25 11:26:50 -0300

    treino React

---
+++ Treino/react-series/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+import App from './components/App';
+
+( 	ReactDOM.render(
+
+	// <h1>Olá Mundo!</h1>,
+	<App />,
+	document.getElementById("app")
+
+)
+);
---
+++ Treino/react-series/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Contador from './Contador';
+
+ export default class App extends React.Component{
+
+	render(){
+
+		return(
+			<div>
+				<Contador/>
+			</div>
+			
+		);
+	};
+}
+
